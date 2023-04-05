edcb3709e74d336958349e597c078031693a4df5
ekiorjckowmfe <129941712+ekiorjckowmfe@users.noreply.github.com>
2023-04-05 10:33:06 +0200

    Initiala filer för MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # . # . .
+        # . . . #
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+})
